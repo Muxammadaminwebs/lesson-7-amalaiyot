3a23c760dbb355a530ca59963d3c2232c88140bc
Muxammadaminwebs <qobiljonovmuxammadamin@gmail.com>
2022-08-24 21:23:36 +0500

    lesson-7 uy ishi

---
+++ JavaScript/style.js
@@ -0,0 +1,25 @@
+"use-strict"
+
+// ===========================1-masala start============================ //
+// let arr = [1, 3, 5, 9];
+// let num = 0;
+// for (let i = 0; i <arr.length; i++){
+//   num+=arr[i]
+// }
+// console.log(num);
+// ===========================1-masala finish============================ //
+
+
+// ===========================2-masala start============================ //
+
+// let stivenNingPuli =+prompt('Qiymatni kiriting')
+// if (stivenNingPuli > 50 && stivenNingPuli < 300) {
+//     console.log(stivenNingPuli+stivenNingPuli*0.15)
+// } else if (stivenNingPuli < 50 ) {
+//     console.log(stivenNingPuli + (stivenNingPuli * 0.2))
+// }else if (stivenNingPuli > 300 ) {
+//     console.log(stivenNingPuli + (stivenNingPuli * 0.2))
+// }
+
+// ===========================2-masala finish============================ //
+
